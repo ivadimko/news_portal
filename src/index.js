9fd4121ab5a5492ef65a65305d7a329ea961618a
vadimilchenko <v.ilchenko@8ways.ch>
2018-06-25 21:48:51 +0300

    change main element render logic

--- src/index.js
+++ src/index.js
@@ -10,15 +10,11 @@ import store from '@/store/createStore';
 
 loadFonts(['Prata:400:latin', 'Montserrat:300,400,700:latin']);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const main = document.createElement('div');
-  main.id = 'app';
-  document.body.insertBefore(main, document.body.firstChild);
-  ReactDOM.render(
+const main = document.getElementById('app');
+ReactDOM.render(
     <Provider store={store}>
       <App/>
     </Provider>,
     main,
-  );
-  Modal.setAppElement(main);
-});
+);
+Modal.setAppElement(main);
